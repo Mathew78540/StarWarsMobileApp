ae8100da88fb170643b86b93c635676b350dd769
Mathieu <mathieu.letyrant@gmail.com>
2015-06-01 00:48:43 +0200

    IOS - Add pages
    - Add all views in Folder Views
    - Add PlayViewController
    - Add YourResultsController

---
+++ IOS/StarWars/StarWars/PlayViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlayViewController.swift
+//  StarWars
+//
+//  Created by Le Tyrant Mathieu on 01/06/2015.
+//  Copyright (c) 2015 BangBang. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ IOS/StarWars/StarWars/YourResultsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  YourResultsViewController.swift
+//  StarWars
+//
+//  Created by Le Tyrant Mathieu on 01/06/2015.
+//  Copyright (c) 2015 BangBang. All rights reserved.
+//
+
+import UIKit
+
+class YourResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
