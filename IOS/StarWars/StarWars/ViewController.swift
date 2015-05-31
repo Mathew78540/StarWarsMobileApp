2e0ba457aa67b6277a1baf8138d344e86ff7dded
Mathieu <mathieu.letyrant@gmail.com>
2015-05-31 19:07:35 +0200

    IOS - Prepare defaultPage
    - Logo
    - BtnPlay
    - BtnHowPlay
    - BtnYourResults

--- IOS/StarWars/StarWars/ViewController.swift
+++ IOS/StarWars/StarWars/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BtnPlay: UIButton!
+    @IBOutlet weak var BtnHowPlay: UIButton!
+    @IBOutlet weak var BtnYourResults: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
