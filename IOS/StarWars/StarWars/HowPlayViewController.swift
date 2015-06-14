9110ec38c4df44243571df5dab422ec7553878b0
Patrick HENG <hengpatrick.pro@gmail.com>
2015-06-14 22:54:15 +0200

    iOs - Home integration

--- IOS/StarWars/StarWars/HowPlayViewController.swift
+++ IOS/StarWars/StarWars/HowPlayViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class HowPlayViewController: UIViewController {
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true);
+        navigationController?.navigationBar.hidden = true; // for navigation bar hide
+        //UIApplication.sharedApplication().statusBarHidden = true; // for status bar hide
+        //navigationController?.navigationBar.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.5); // Don't work
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- IOS/StarWars/StarWars/ViewController.swift
+++ IOS/StarWars/StarWars/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var BtnHowPlay: UIButton!
     @IBOutlet weak var BtnYourResults: UIButton!
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true);
+        navigationController?.navigationBar.hidden = true; // for navigation bar hide
+        //UIApplication.sharedApplication().statusBarHidden = true; // for status bar hide
+        //navigationController?.navigationBar.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.5); // Don't work
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
